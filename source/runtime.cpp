1bd06c40bc74684e147451f63b53bf010120c130
liuxd17 <lxd21@mails.tsinghua.edu.cn>
2024-01-01 10:18:48 +0800

    disable update check

--- source/runtime.cpp
+++ source/runtime.cpp
@@ -322,7 +322,7 @@ reshade::runtime::runtime(api::swapchain *swapchain, api::command_queue *graphic
 	else
 		LOG(INFO) << "Running on " << device_description << '.';
 
-	check_for_update();
+	// check_for_update();
 
 	// Default shortcut PrtScrn
 	_screenshot_key_data[0] = 0x2C;
