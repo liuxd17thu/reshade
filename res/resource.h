4b446ddc8dd4948cfc6ba92d98f9ef6927f6d0d2
liuxd17 <lxd21@mails.tsinghua.edu.cn>
2024-02-15 23:53:04 +0800

    Icon refresh

--- res/resource.h
+++ res/resource.h
@@ -27,10 +27,10 @@
 #define IDR_LICENSE_VULKAN              711
 
 // Next default values for new objects
-// 
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        113
+#define _APS_NEXT_RESOURCE_VALUE        121
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           113
